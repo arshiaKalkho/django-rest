d1e32b6e379a5a47eddcedbcbc81934341be121d
arshia <arshiabdol@yahoo.com>
2022-02-16 17:41:33 -0500

    added html logic

--- main/views.py
+++ main/views.py
@@ -4,8 +4,8 @@ from .models import ToDoList, Item
 
 def index(response, id):
     list = ToDoList.objects.get(id=id)
-    return render(response, "main/base.html", {"name":list.name})
+    return render(response, "main/list.html", {"list":list})
 
 def home(response):
-    return render(response, "main/home.html", {"name": 'no value set'})
+    return render(response, "main/home.html",{})
     
