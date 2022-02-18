b31b34dee6996bcc3c25323646a1d6e1783d2cc6
arshia <arshiabdol@yahoo.com>
2022-02-17 21:03:25 -0500

    post logic completed and tested

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from sqlite3 import complete_statement
+from webbrowser import get
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import ToDoList, Item
@@ -5,6 +7,27 @@ from .forms import CreateNewList
 
 def index(response, id):
     list = ToDoList.objects.get(id=id)
+    
+    if response.method == "POST":
+        print("received:    ",response.POST)
+        if response.POST.get('save'):#update new item if are completed or not 
+            for item in list.item_set.all():
+                if response.POST.get('c' + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+
+        elif response.POST.get('newItem'):
+            txt = response.POST.get('new')
+            print("text: ",txt)
+            if len(txt) > 2: #minlength of two
+                list.item_set.create(text=txt, complete=False)
+                
+            else:
+                print('inavlid input')
+        
+        
     return render(response, "main/list.html", {"list":list})
 
 def home(response):
